b63aafde8c1d3c808752b8ab3d39c9c1183152aa
Thomas Elton <thomaselton.au@gmail.com>
2024-06-07 23:45:26 +1000

    Updated formatting

--- mod-data.R
+++ mod-data.R
@@ -19,69 +19,76 @@ dataUploadUI <- function(id) {
       style = "color:black; background:#c8cacc"
     ),
     
-
-    tabsetPanel(
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "navpill_styles.css")
+    ),
+    
+    card(
       
-      ### Page to upload the data. ###
-      # This page will include a file input button.
-      # When the file is uploaded, a data table viewer of the original data.
-      tabPanel("Step 1) Data Upload",
+      tabsetPanel(
         
-         HTML("<br>"),
-         p("Some information will be placed here about how to upload the data set. Possible more instructions... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pharetra et nibh ac varius. Quisque dapibus consectetur ex. Fusce sit amet dui erat. Sed tellus elit, tempor vel egestas a, viverra nec erat. Nullam nec felis posuere, faucibus justo eu, luctus eros. Cras consequat mauris sed ante lacinia, ut lobortis lorem dignissim. Nunc elementum rhoncus ex, nec pulvinar ex ullamcorper et. Praesent sodales, lorem nec fermentum pretium, tortor purus vestibulum arcu, eu egestas turpis est nec dui."),
-         HTML("<br>"),
-         sidebarPanel(
-           fileInput(ns("file"), "File input:")
-         ),
-         mainPanel(
-           tableOutput(ns("table"))
-         )
-         
+        type = "pills",
+        
+        ### Page to upload the data. ###
+        # This page will include a file input button.
+        # When the file is uploaded, a data table viewer of the original data.
+        tabPanel("Step 1) Data Upload",
+                 
+                 HTML("<br>"),
+                 p("Some information will be placed here about how to upload the data set. Possible more instructions... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pharetra et nibh ac varius. Quisque dapibus consectetur ex. Fusce sit amet dui erat. Sed tellus elit, tempor vel egestas a, viverra nec erat. Nullam nec felis posuere, faucibus justo eu, luctus eros. Cras consequat mauris sed ante lacinia, ut lobortis lorem dignissim. Nunc elementum rhoncus ex, nec pulvinar ex ullamcorper et. Praesent sodales, lorem nec fermentum pretium, tortor purus vestibulum arcu, eu egestas turpis est nec dui."),
+                 HTML("<br>"),
+                 sidebarPanel(
+                   fileInput(ns("file"), "File input:")
+                 ),
+                 mainPanel(
+                   tableOutput(ns("table"))
+                 )
+                 
+        ),
+        
+        ### Page to start cleaning the data ###
+        # Option to select a variable that appears in the data set.
+        # Then can change the classification of the variable and do some data filtering/cleaning.
+        tabPanel("Step 2) Data Cleaning",
+                 HTML("<br>"),
+                 p("Some information here on data cleaning... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pharetra et nibh ac varius. Quisque dapibus consectetur ex. Fusce sit amet dui erat. Sed tellus elit, tempor vel egestas a, viverra nec erat. Nullam nec felis posuere, faucibus justo eu, luctus eros. Cras consequat mauris sed ante lacinia, ut lobortis lorem dignissim. Nunc elementum rhoncus ex, nec pulvinar ex ullamcorper et. Praesent sodales, lorem nec fermentum pretium, tortor purus vestibulum arcu, eu egestas turpis est nec dui."),
+                 HTML("<br>"),
+                 
+                 
+                 fluidRow(
+                   
+                   # Side bar with buttons.
+                   column(4, 
+                          h3("Column 1"),
+                          p("This is column 1."),
+                          bslib::card(
+                            card_header("Card header"),
+                            "Card body"
+                          )
+                   ),
+                   
+                   
+                   column(8, 
+                          h3("Column 2"),
+                          p("This is column 2")
+                   ),
+                   
+                 ),
+                 
+                 # sidebarPanel(
+                 #   uiOutput(outputId = ns("variable_names_button")),
+                 #   uiOutput(outputId = ns("column_data_type"))
+                 # ),
+                 # mainPanel(
+                 #   DTOutput(outputId = ns("column_output"))
+                 # )
+        )
       ),
       
-      ### Page to start cleaning the data ###
-      # Option to select a variable that appears in the data set.
-      # Then can change the classification of the variable and do some data filtering/cleaning.
-      tabPanel("Step 2) Data Cleaning",
-               HTML("<br>"),
-               p("Some information here on data cleaning... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pharetra et nibh ac varius. Quisque dapibus consectetur ex. Fusce sit amet dui erat. Sed tellus elit, tempor vel egestas a, viverra nec erat. Nullam nec felis posuere, faucibus justo eu, luctus eros. Cras consequat mauris sed ante lacinia, ut lobortis lorem dignissim. Nunc elementum rhoncus ex, nec pulvinar ex ullamcorper et. Praesent sodales, lorem nec fermentum pretium, tortor purus vestibulum arcu, eu egestas turpis est nec dui."),
-               HTML("<br>"),
-               
-               
-               fluidRow(
-                 
-                 # Side bar with buttons.
-                 column(4, 
-                        h3("Column 1"),
-                        p("This is column 1."),
-                        bslib::card(
-                          card_header("Card header"),
-                          "Card body"
-                        )
-                 ),
-                 
-                 
-                 column(8, 
-                        h3("Column 2"),
-                        p("This is column 2")
-                 ),
-                 
-                 
-                 
-                 
-                 
-                 
-               ),
-               
-               # sidebarPanel(
-               #   uiOutput(outputId = ns("variable_names_button")),
-               #   uiOutput(outputId = ns("column_data_type"))
-               # ),
-               # mainPanel(
-               #   DTOutput(outputId = ns("column_output"))
-               # )
-      )
-    )
+      style = "color:black; background:#FFFFFF"
+    ),
+
+
   )
 }
 
