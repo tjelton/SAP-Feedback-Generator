4499867ff63209234fe6d72a848525af9eced308
Thomas Elton <thomaselton.au@gmail.com>
2024-06-10 17:07:33 +1000

    When data is uplodaded, it displays to the screen using a conditional panel.

--- mod-data.R
+++ mod-data.R
@@ -34,46 +34,53 @@ dataUploadUI <- function(id) {
         # When the file is uploaded, a data table viewer of the original data.
         tabPanel("Step 1) Data Upload",
                 
-                 HTML('<hr style="border: 0; border-top: 2px solid #232324; margin: 10px 0 15px 0;">'),
-                 
-                 # Info for how to upload the file and upload button.
-                 fluidRow(
-                   
-                   # Information
-                   column(8,
-                      card(
-                        height = 200,
-                        HTML("
-                          <p>File upload requirements:<br>
-                          <ul>
-                            <li>Only excel files (extension .xlsx) are currently supported.</li>
-                            <li>The excel file must have the survey data on a tab named \"Form Responses 1\".</li>
-                            <li>The first row of the spreadsheet must contain the column names, and each row underneath represents the responses from
-                            a single person.</li>
-                            <li>More file upload types coming soon...</li>
-                          </ul></p>"
-                        ),
-                      )
-                   ),
-                   
-                   # Upload
-                   column(4,
-                      br(),
-                      card(
-                        fileInput(ns("file"), "File input:"),
-                        style = "background:#cce4fc" # Light blue colour
-                      )
-                   )
-                   
-                 ),
+           HTML('<hr style="border: 0; border-top: 2px solid #232324; margin: 10px 0 15px 0;">'),
+           
+           # Info for how to upload the file and upload button.
+           fluidRow(
+             
+             # Information
+             column(8,
+                card(
+                  height = 270,
+                  HTML("
+                    <p>File upload requirements:<br>
+                    <ul>
+                      <li>Only excel files (extension .xlsx) are currently supported.</li>
+                      <li>The excel file must have the survey data on a tab named \"Form Responses 1\".</li>
+                      <li>The first row of the spreadsheet must contain the column names, and each row underneath represents the responses from
+                      a single person.</li>
+                      <li>More file upload types coming soon...</li>
+                      <li>Once uploaded, the file contents will be displayed in the table below. This is the original data as you uploaded it (it will
+                      not contains the updates that you make in the following stages).</li>
+                    </ul></p>"
+                  ),
+                )
+             ),
+             
+             # Upload
+             column(4,
+                br(),
+                br(),
+                card(
+                  HTML("<p><b>File Input</b></p>"),
+                  fileInput(ns("data_file"), ""),
+                  style = "background:#cce4fc" # Light blue colour
+                )
+             )
+             
+           ),
+           
+           conditionalPanel(
+             
+             condition = paste0("output['", ns('data_uploaded_flag'), "']"),
+             h5("Uploaded Data:"),
+             tableOutput(ns("table"))
+             
+           ),
+           
 
-                 HTML("<br>"),
-                 sidebarPanel(
-                   p("TEST")
-                 ),
-                 mainPanel(
-                   tableOutput(ns("table"))
-                 )
+           
                  
         ),
         
@@ -81,38 +88,38 @@ dataUploadUI <- function(id) {
         # Option to select a variable that appears in the data set.
         # Then can change the classification of the variable and do some data filtering/cleaning.
         tabPanel("Step 2) Data Cleaning",
-                 HTML("<br>"),
-                 p("Some information here on data cleaning... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pharetra et nibh ac varius. Quisque dapibus consectetur ex. Fusce sit amet dui erat. Sed tellus elit, tempor vel egestas a, viverra nec erat. Nullam nec felis posuere, faucibus justo eu, luctus eros. Cras consequat mauris sed ante lacinia, ut lobortis lorem dignissim. Nunc elementum rhoncus ex, nec pulvinar ex ullamcorper et. Praesent sodales, lorem nec fermentum pretium, tortor purus vestibulum arcu, eu egestas turpis est nec dui."),
-                 HTML("<br>"),
-                 
-                 
-                 fluidRow(
-                   
-                   # Side bar with buttons.
-                   column(4, 
-                          h3("Column 1"),
-                          p("This is column 1."),
-                          bslib::card(
-                            card_header("Card header"),
-                            "Card body"
-                          )
-                   ),
-                   
-                   
-                   column(8, 
-                          h3("Column 2"),
-                          p("This is column 2")
-                   ),
-                   
-                 ),
-                 
-                 # sidebarPanel(
-                 #   uiOutput(outputId = ns("variable_names_button")),
-                 #   uiOutput(outputId = ns("column_data_type"))
-                 # ),
-                 # mainPanel(
-                 #   DTOutput(outputId = ns("column_output"))
-                 # )
+           HTML("<br>"),
+           p("Some information here on data cleaning... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pharetra et nibh ac varius. Quisque dapibus consectetur ex. Fusce sit amet dui erat. Sed tellus elit, tempor vel egestas a, viverra nec erat. Nullam nec felis posuere, faucibus justo eu, luctus eros. Cras consequat mauris sed ante lacinia, ut lobortis lorem dignissim. Nunc elementum rhoncus ex, nec pulvinar ex ullamcorper et. Praesent sodales, lorem nec fermentum pretium, tortor purus vestibulum arcu, eu egestas turpis est nec dui."),
+           HTML("<br>"),
+           
+           
+           fluidRow(
+             
+             # Side bar with buttons.
+             column(4, 
+                    h3("Column 1"),
+                    p("This is column 1."),
+                    bslib::card(
+                      card_header("Card header"),
+                      "Card body"
+                    )
+             ),
+             
+             
+             column(8, 
+                    h3("Column 2"),
+                    p("This is column 2")
+             ),
+             
+           ),
+           
+           # sidebarPanel(
+           #   uiOutput(outputId = ns("variable_names_button")),
+           #   uiOutput(outputId = ns("column_data_type"))
+           # ),
+           # mainPanel(
+           #   DTOutput(outputId = ns("column_output"))
+           # )
         )
       ),
       
@@ -130,17 +137,26 @@ dataUploadServer <- function(id) {
     
     # Reactive expression to read the uploaded file
     original_data <- reactive({
-      req(input$file)  # Ensure a file is uploaded
-      infile <- input$file
+      if(is.null(input$data_file)) return(NULL)
+      req(input$data_file)  # Ensure a file is uploaded
+      infile <- input$data_file
       data <- read_excel(infile$datapath, sheet = "Form Responses 1")
       return(data)
     })
     
-    # Data sore for data that can be modified.
+    # Data store for data that can be modified.
     modified_data <- reactive({
       req(original_data())
       return(original_data())
     })
+    
+    # Flag for when data has been uploaded.
+    output$data_uploaded_flag <- reactive({
+      val <- !(is.null(input$data_file))
+      print(val)
+    })
+    # To make the flag output reactive to be accessed from the ui.
+    outputOptions(output, 'data_uploaded_flag', suspendWhenHidden=FALSE)
     
     # Render the processed data as a table
     output$table <- renderTable({
