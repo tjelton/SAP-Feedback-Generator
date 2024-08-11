a7a75d68e4ae2278e21e7dbf363db7eef9e1c795
Thomas Elton <thomaselton.au@gmail.com>
2024-08-11 20:04:45 +1000

    Remove test content from the home page. Tidying before making respoistory public.

--- app.R
+++ app.R
@@ -13,8 +13,6 @@ library(bslib)
 
 data <- read_excel("Student Feedback (Semester 1 2024) (Responses).xlsx", sheet = "Form Responses 1")
 
-
-
 ui  <- tagList(
   navbarPage(
     
@@ -27,33 +25,7 @@ ui  <- tagList(
       # Tab Title
       HTML('<h7 style="padding-left: 10px; padding-right: 10px; display: inline;">Page 1</h5>'),
 
-      sidebarPanel(
-               fileInput("file", "File input:"),
-               textInput("txt", "Text input:", "general"),
-               sliderInput("slider", "Slider input:", 1, 100, 30),
-               tags$h5("Default actionButton:"),
-               actionButton("action", "Search"),
-               
-               tags$h5("actionButton with CSS class:"),
-               actionButton("action2", "Action button", class = "btn-primary")
-             ),
-             mainPanel(
-               tabsetPanel(
-                 tabPanel("Tab 1",
-                          h4("Table"),
-                          tableOutput("table"),
-                          h4("Verbatim text output"),
-                          verbatimTextOutput("txtout"),
-                          h1("Header 1"),
-                          h2("Header 2"),
-                          h3("Header 3"),
-                          h4("Header 4"),
-                          h5("Header 5")
-                 ),
-                 tabPanel("Tab 2", "This panel is intentionally left blank"),
-                 tabPanel("Tab 3", "This panel is intentionally left blank")
-               )
-             )
+      "Home page"
     ),
     
     
