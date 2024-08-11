69bab2f1ea6bd67331873967aa2fde76b6a256fd
Thomas Elton <thomaselton.au@gmail.com>
2024-08-11 20:18:47 +1000

    Added srv and ui files for analysis portion of the app.

---
+++ mod_analysis_srv.R
@@ -0,0 +1,8 @@
+
+
+analysisServer <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+    
+  })
+}
---
+++ mod_analysis_ui.R
@@ -0,0 +1,8 @@
+
+
+analysisUI <- function(id) {
+  ns <- NS(id)
+  tagList(
+    p("Analysis UI!")
+  )
+}
